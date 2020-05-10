5bc6aacac15af4534ea5c77a58747fbef8233fc2
wadewhy <wadewhy@foxmail.com>
2020-05-10 22:36:50 +0800

    springcloud搭建和eureka单击集群搭建案例

--- cloud-provider-payment8002/src/main/java/xyz/wadewhy/springcloud/PaymentMain8002.java
+++ cloud-provider-payment8002/src/main/java/xyz/wadewhy/springcloud/PaymentMain8002.java
@@ -19,6 +19,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class PaymentMain8002 {
     public static void main(String[] args) {
+
         SpringApplication.run(PaymentMain8002.class);
     }
 }
